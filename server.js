a44575672f95e9dd33f2255d4448c179d8aaad61
Danila A. Krysov <Tomahawk.RUS@gmail.com>
2024-03-11 00:03:50 +0700

    Second commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\nMy First Node Server\n');
+  res.end('Hello World\nMy First Node Server\nI modified it');
 });
 
 server.listen(port, hostname, () => {
